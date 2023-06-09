7557cb7deff6613cec4b8aba4aed402f66254df6
roll-t <phuoctruong727@gmail.com>
2023-06-09 14:56:00 +0700

    them trang dang ky dang nhap

--- js/header/header_site/header_sticky.js
+++ js/header/header_site/header_sticky.js
@@ -1,7 +1,8 @@
 class SticKyHeader{
     constructor(){
         this.stickyHeader=document.querySelector('.header .site-header')
-        this.banner=document.querySelector('.header .banner')
+        this.banner=document.querySelector('.banner')
+        this.toTop=document.querySelector('.to-top')
         this.heightSiteHeader=86;
         this.contact=40;
         this.space=this.heightSiteHeader+this.contact
@@ -17,10 +18,12 @@ class SticKyHeader{
         })
     }
     stickyIn(){
+        this.toTop.classList.add('active')
         this.stickyHeader.classList.add('sticky')
         this.banner.style.marginTop=`${this.heightSiteHeader}px`
     }
     stickyOut(){
+        this.toTop.classList.remove('active')
         this.banner.style.marginTop=`0px`;
         this.stickyHeader.classList.remove('sticky')
      
